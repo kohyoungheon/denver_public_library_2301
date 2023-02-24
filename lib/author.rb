1d9da66ab764eef1615829ad12ba688d7afd9cd4
Young Koh <youngheon6@gmail.com>
2023-02-24 09:41:17 -0700

    feat: Define and test Library class

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,17 @@
 class Author
+  attr_reader :name, :books, :first_name, :last_name
+  attr_writer :books
 
+  def initialize(infohash)
+    @firstname = infohash[:first_name]
+    @last_name = infohash[:last_name]
+    @name = @firstname + ' ' + @last_name
+    @books = []
+  end
+
+  def write(title,date)
+    book = Book.new({author_first_name: @firstname, author_last_name: @last_name, title: title, publication_date: date})
+    @books << book
+    book
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,9 +1,35 @@
-# RSpec.describe Author do
-#   describe "#initialize" do
-#     it "is an instance of _" do
+require './lib/author'
 
-#     end
+RSpec.describe Author do
+  before(:each) do
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
 
-#     it "has attributes" do
-#   end
-# end
+  describe "#initialize" do
+    it "is an instance of Author" do
+      expect(@charlotte_bronte).to be_instance_of(Author)
+    end
+
+    it "has attributes" do
+      expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
+      expect(@charlotte_bronte.books).to eq([])
+    end
+  end
+
+  describe "#write" do
+    it "creates a book under the author" do
+      jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+      expect(jane_eyre.class).to eq(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+
+      villette = @charlotte_bronte.write("Villette", "1853")
+      expect(villette.class).to eq(Book)
+      expect(villette.title).to eq("Villette")
+
+      expect(@charlotte_bronte.books.length).to eq(2)
+      expect(@charlotte_bronte.books.include?(jane_eyre)).to eq(true)
+      expect(@charlotte_bronte.books.include?(villette)).to eq(true)
+    end
+  end
+end
