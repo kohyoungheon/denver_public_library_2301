5219af8114697d814ed15bab7b1bf9c40c11c9de
Young Koh <youngheon6@gmail.com>
2023-02-24 10:02:04 -0700

    feat: #add_author

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,17 @@
 class Library
- 
+  attr_reader :name, :books, :authors
+  attr_writer :books, :authors
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,9 +1,42 @@
-# RSpec.describe Library do
-#   describe "#initialize" do
-#     it "is an instance of _" do
+require './lib/library'
 
-#     end
+RSpec.describe Library do
+  before(:each) do
+    @library = Library.new("Denver Library")
 
-#     it "has attributes" do
-#   end
-# end
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "March 10, 1853")
+
+    @bob= Author.new({first_name: "Bob", last_name: "Bobson"})
+    @bobs_novel = @bob.write("Bob's Novel", "December 11, 2021")
+    @bobs_poems = @bob.write("Bob's Poems", "June 22, 2022")
+  end
+
+  describe "#initialize" do
+    it "is an instance of Library" do
+      expect(@library).to be_instance_of(Library)
+    end
+
+    it "has attributes" do
+      expect(@library.name).to eq("Denver Library")
+      expect(@library.books).to eq([])
+      expect(@library.authors).to eq([])
+    end
+  end
+
+  describe "#add_author" do
+    it "stores author and all books author has written" do
+      expect(@library.authors).to eq([])
+      expect(@library.books).to eq([])
+
+      @library.add_author(@charlotte_bronte)
+      expect(@library.authors).to eq([@charlotte_bronte])
+      expect(@library.books).to eq([@jane_eyre, @villette])
+
+      @library.add_author(@bob)
+      expect(@library.authors).to eq([@charlotte_bronte, @bob])
+      expect(@library.books).to eq([@jane_eyre, @villette, @bobs_novel, @bobs_poems])
+    end
+  end
+end
